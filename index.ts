9666771f0d446a61a6dbafabe29c22518c4ba912
nguyenquochuy2311 <backend-team@cubable.com>
2024-12-16 09:57:56 +0700

    Refactors model imports
    Improves code organization by moving model imports to their respective files.  This improves maintainability and readability.

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import { FieldDataType } from './src/field-datatype';
 import * as FieldType from './src/fields';
-import { Models } from './src/models';
+import * as _FieldModel from './src/models/field';
+import * as _RecordDataModel from './src/models/record-data';
+import * as _RecordMetaModel from './src/models/record-meta';
 import { _FieldRepository } from './src/repositories/field.repository';
 import { _RecordDataRepository } from './src/repositories/record-data.repository';
 import { _RecordMetaRepository } from './src/repositories/record-meta.repository';
 import { getTableConnection, initTableConnection } from './src/table-connection';
 
-export { _FieldRepository, _RecordDataRepository, _RecordMetaRepository, FieldDataType, FieldType, getTableConnection, initTableConnection, Models };
+export {
+	_FieldModel,
+	_FieldRepository,
+	_RecordDataModel,
+	_RecordDataRepository,
+	_RecordMetaModel,
+	_RecordMetaRepository,
+	FieldDataType,
+	FieldType,
+	getTableConnection,
+	initTableConnection
+};
 
 (async () => {
 	await initTableConnection({
