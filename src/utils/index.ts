cbd31b4f74196e4fa7aaaafdccb0713e78395abe
Backend Team <backend-team@cubable.com>
2024-12-28 17:29:33 +0700

    Renames table IDs in utils
    Updates the `parseTableID` function in the utils module to prefix table IDs with `RecordData_` instead of `Table_`.  This improves data clarity and consistency.
    
    Refactors `.gitignore` to remove redundant entry.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import type { ULID } from 'ulidx';
 
-export const parseTableID = (tableID: ULID) => `Table_${tableID}`;
+export const parseTableID = (tableID: ULID) => `RecordData_${tableID}`;
 
 export const catchError = async <T>(promise: Promise<T>): Promise<[undefined, T] | [Error]> => {
 	return promise
